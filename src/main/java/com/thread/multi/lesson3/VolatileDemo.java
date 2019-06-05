e0ff22e2ab39e36ce28e81aa04ba05314c558b68
rogan.luo <rogan.luo@corp.to8to.com>
2019-06-05 23:27:26 +0800

    volatile理解

--- src/main/java/com/thread/multi/lesson3/VolatileDemo.java
+++ src/main/java/com/thread/multi/lesson3/VolatileDemo.java
@@ -1,27 +1,29 @@
 package com.thread.multi.lesson3;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by rogan on 2019/5/28.
  */
 public class VolatileDemo {
-    public static /*volatile*/ boolean stop = false;
+    public volatile static boolean stop = false;
 
-    //volatile 使用
+    /**
+     * volatile 使用与不使用的区别
+     */
     public static void main(String[] args) throws InterruptedException {
         Thread thread =new Thread(()->{
             int i=0;
             while (!stop)
             {
-                System.out.println(i++);
+                i++;
             }
         });
         thread.start();
-        System.out.println("begin start thread");
         TimeUnit.SECONDS.sleep(1);
         stop = true;
-
     }
-
 }
